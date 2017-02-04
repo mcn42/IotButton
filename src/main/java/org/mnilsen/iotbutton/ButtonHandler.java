dca7e3f34d7de6d5f1b0603698e25cb932942fd8
Michael Nilsen <michael.nilsen@gmail.com>
2017-02-04 10:40:22 -0500

    new

---
+++ src/main/java/org/mnilsen/iotbutton/ButtonHandler.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.mnilsen.iotbutton;
+
+import com.amazonaws.services.lambda.runtime.Context;
+
+/**
+ *
+ * @author michaeln
+ */
+public class ButtonHandler {
+
+    public ButtonHandler() {
+    }
+    
+    public void handleEvent(String data, Context ctx)
+    {
+        
+    }
+}
